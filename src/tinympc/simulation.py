9c6aab46c0a7772cdb80b7a3015e33df13e8c554
lzx <lzx@lzx-3.local>
2024-05-22 21:37:29 +0800

    asdf

--- src/tinympc/simulation.py
+++ src/tinympc/simulation.py
@@ -2,6 +2,6 @@ import torch
 import simulation
 
 test_input = torch.randn([1,2]).cuda()
-# print("test_input = " , test_input)
+print("test_input = " , test_input)
 max_val = simulation.simulation(test_input)
 print(max_val)
