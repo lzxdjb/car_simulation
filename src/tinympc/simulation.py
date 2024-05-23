47d0d68937a147db62e63918ca88e739f6acd412
lzxdjb <lzxdjb@berkeley.edu>
2024-05-23 08:46:46 +0800

    asdfasdf

--- src/tinympc/simulation.py
+++ src/tinympc/simulation.py
@@ -1,7 +1,7 @@
 import torch
-import simulation
+import graph_max
 
 test_input = torch.randn([1,2]).cuda()
 print("test_input = " , test_input)
-max_val = simulation.simulation(test_input)
+max_val = graph_max.graph_max(test_input)
 print(max_val)
